7e8105d1013a13f375a7e67ec3552a99d0b57bde
YosefPalma <yosefpapu6@gmail.com>
2024-02-05 12:38:37 -0600

    primer commit

---
+++ lib/login_screen.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
+import 'package:flutter_login/flutter_login.dart';
+import 'package:edupay/constants.dart';
+import 'package:edupay/custom_route.dart';
+import 'package:edupay/users.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'Menulateral.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const routeName = '/auth';
+
+  const LoginScreen({super.key});
+
+  Duration get loginTime => Duration(milliseconds: timeDilation.ceil() * 2250);
+
+  Future<String?> _loginUser(LoginData data) {
+    return Future.delayed(loginTime).then((_) {
+      if (!mockUsers.containsKey(data.name)) {
+        return 'User not exists';
+      }
+      if (mockUsers[data.name] != data.password) {
+        return 'Password does not match';
+      }
+      return null;
+    });
+  }
+
+  Future<String?> _signupUser(SignupData data) {
+    return Future.delayed(loginTime).then((_) {
+      return null;
+    });
+  }
+
+  Future<String?> _recoverPassword(String name) {
+    return Future.delayed(loginTime).then((_) {
+      if (!mockUsers.containsKey(name)) {
+        return 'User not exists';
+      }
+      return null;
+    });
+  }
+
+  Future<String?> _signupConfirm(String error, LoginData data) {
+    return Future.delayed(loginTime).then((_) {
+      return null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+      logo: const AssetImage('img/Edupay.png'),
+      logoTag: Constants.logoTag,
+      titleTag: Constants.titleTag,
+      navigateBackAfterRecovery: true,
+      onConfirmRecover: _signupConfirm,
+      onConfirmSignup: _signupConfirm,
+      loginAfterSignUp: false,
+      termsOfService: [
+        TermOfService(
+          id: 'newsletter',
+          mandatory: false,
+          text: 'Newsletter subscription',
+        ),
+        TermOfService(
+          id: 'general-term',
+          mandatory: true,
+          text: 'Term of services',
+          linkUrl: 'https://github.com/YosefPalma/flutter_login.git',
+        ),
+      ],
+      additionalSignupFields: [
+        const UserFormField(
+          keyName: 'Username',
+          icon: Icon(FontAwesomeIcons.userLarge),
+        ),
+        const UserFormField(keyName: 'Name'),
+        const UserFormField(keyName: 'Surname'),
+        UserFormField(
+          keyName: 'phone_number',
+          displayName: 'Phone Number',
+          userType: LoginUserType.phone,
+          fieldValidator: (value) {
+            final phoneRegExp = RegExp(
+              '^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}\$',
+            );
+            if (value != null &&
+                value.length < 7 &&
+                !phoneRegExp.hasMatch(value)) {
+              return "This isn't a valid phone number";
+            }
+            return null;
+          },
+        ),
+      ],
+      userValidator: (value) {
+        if (!value!.contains('@') || !value.endsWith('.com')) {
+          return "Email must contain '@' and end with '.com'";
+        }
+        return null;
+      },
+      passwordValidator: (value) {
+        if (value!.isEmpty) {
+          return 'Password is empty';
+        }
+        return null;
+      },
+      onLogin: (loginData) {
+        debugPrint('Login info');
+        debugPrint('Name: ${loginData.name}');
+        debugPrint('Password: ${loginData.password}');
+        return _loginUser(loginData);
+      },
+
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(
+          FadePageRoute(
+            builder: (context) => MyApp3(),
+          ),
+        );
+      },
+      onRecoverPassword: (name) {
+        debugPrint('Recover password info');
+        debugPrint('Name: $name');
+        return _recoverPassword(name);
+        // Show new password dialog
+      },
+      headerWidget: const IntroWidget(),
+    );
+  }
+}
+
+class IntroWidget extends StatelessWidget {
+  const IntroWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Row(
+          children: <Widget>[
+            Expanded(child: Divider()),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("Inicia sesion"),
+            ),
+            Expanded(child: Divider()),
+          ],
+        ),
+      ],
+    );
+  }
+}
